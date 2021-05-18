1ddbcb56fd02d5d2daf2611c94015be3ec3ecf14
mox93 <mohamed.ragaiy.saleh@gmail.com>
2021-05-18 07:13:10 +0200

    Renamed file from main.py to dynamic_variavles.py

--- cocoon/dynamic_variables.py
+++ cocoon/dynamic_variables.py
@@ -1,14 +1,6 @@
 from cocoon.token_ import Token
 
 
-# TODO: (use cases)
-#  - pass a token for None so it doesn't get filter out by FastAPI. [DONE]
-#  - generate a new fake value for each token instance. [DONE]
-#  - have multiple proxy instances share the same fake value. [DONE]
-#  - define custom proxies. [DONE]
-#  - configure the Faker instance used for generating values. [DONE]
-
-
 # For use in the Field example argument instead of <None> because
 # FastAPI ignores it
 # >>> ... address: str = Field(None, example=NONE)
