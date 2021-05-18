87525df004fb9cd0e1e8ef0c0fa2d7031cc580df
mox93 <mohamed.ragaiy.saleh@gmail.com>
2021-05-18 07:08:07 +0200

    Replace dict & (list, tuple, set) with Mapping & Iterable

--- cocoon/parser.py
+++ cocoon/parser.py
@@ -1,16 +1,26 @@
+from typing import Mapping, Iterable
+
 from cocoon.token_ import Token
 
 
-def deep_inject(obj):
+def parse(obj):
+    """
+    recursively loop over elements and parse any instance of a string or a
+    :param obj:
+    :return: a new object with all placeholders replaced with the
+     appropriate value.
+    """
     if isinstance(obj, (str, Token)):
         return Token.parse(obj)
 
-    if isinstance(obj, dict):
-        return {
-            key: deep_inject(value) for key, value in obj.items()
-        }
+    type_ = obj.__class__
 
-    if isinstance(obj, (list, tuple, set)):
-        return obj.__class__(deep_inject(value) for value in obj)
+    if isinstance(obj, Mapping):
+        return type_(
+            (key, parse(value)) for key, value in obj.items()
+        )
+
+    if isinstance(obj, Iterable):
+        return type_(parse(value) for value in obj)
 
     return obj
