a510b0f97026c966e63dbdae65258ab8fa697231
mox93 <mohamed.ragaiy.saleh@gmail.com>
2021-05-18 07:17:10 +0200

    Added meta data and comments

--- cocoon/__init__.py
+++ cocoon/__init__.py
@@ -1,9 +1,24 @@
-from cocoon.main import (
-    ADDRESS,
-    FIRST_NAME,
-    NAME,
-    NONE,
-    TEXT,
-)
-from cocoon.parser import deep_inject
+from cocoon.parser import parse
 from cocoon.token_ import Token
+
+
+__author__ = "Mohamed Ragaey Saleh"
+__license__ = "MIT"
+__version__ = "0.1.0"
+__maintainer__ = "Mohamed Ragaey Saleh"
+__email__ = "mohamed.ragaiy.saleh@gmail.com"
+__status__ = "Prototype"
+
+
+__all__ = (
+    parser,
+    Token,
+)
+
+
+# TODO: (use cases)
+#  - pass a token for None so it doesn't get filter out by FastAPI. [DONE]
+#  - generate a new fake value for each token instance. [DONE]
+#  - have multiple token instances share the same fake value. [DONE]
+#  - define custom proxies. [DONE]
+#  - configure the Faker instance used for generating values. [DONE]
