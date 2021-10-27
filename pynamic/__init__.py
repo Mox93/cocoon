b4529865880c10e92890319c71128347ca173f27
mox93 <mohamed.ragaiy.saleh@gmail.com>
2021-10-27 04:44:18 +0200

    Updated package version to 0.3.0

--- pynamic/__init__.py
+++ pynamic/__init__.py
@@ -4,7 +4,7 @@ from .token import Token
 
 __author__ = "Mohamed Ragaey Saleh"
 __license__ = "MIT"
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 __maintainer__ = "Mohamed Ragaey Saleh"
 __email__ = "mohamed.ragaiy.saleh@gmail.com"
 __status__ = "Development"
